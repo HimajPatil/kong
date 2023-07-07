0537182051f5304005de0bbbf39ef1e06b1d2579
chronolaw <chrono_cpp@me.com>
2023-07-07 20:34:27 +0800

    01-proxy_spec.lua

--- spec/02-integration/05-proxy/01-proxy_spec.lua
+++ spec/02-integration/05-proxy/01-proxy_spec.lua
@@ -2,6 +2,7 @@ local helpers = require "spec.helpers"
 local utils = require "pl.utils"
 local stringx = require "pl.stringx"
 local http = require "resty.http"
+local atc_compat = require "kong.router.compat"
 
 
 local function count_server_blocks(filename)
@@ -136,9 +137,48 @@ describe("#stream proxy interface listeners", function()
   end)
 end)
 
+local function reload_router(flavor)
+  _G.kong = {
+    configuration = {
+      router_flavor = flavor,
+    },
+  }
+
+  helpers.setenv("KONG_ROUTER_FLAVOR", flavor)
+
+  package.loaded["spec.helpers"] = nil
+  package.loaded["kong.global"] = nil
+  package.loaded["kong.cache"] = nil
+  package.loaded["kong.db"] = nil
+  package.loaded["kong.db.schema.entities.routes"] = nil
+  package.loaded["kong.db.schema.entities.routes_subschemas"] = nil
+
+  helpers = require "spec.helpers"
+
+  helpers.unsetenv("KONG_ROUTER_FLAVOR")
+end
+
+
+local function gen_route(flavor, r)
+  if flavor ~= "expressions" then
+    return r
+  end
+
+  r.expression = atc_compat.get_expression(r)
+  r.priority = tonumber(atc_compat._get_priority(r))
+
+  r.destinations = nil
+
+  return r
+end
+
+
+for _, flavor in ipairs({ "traditional", "traditional_compatible", "expressions" }) do
 for _, strategy in helpers.each_strategy() do
   if strategy ~= "off" then
-    describe("[stream]", function()
+    describe("[stream" .. ", flavor = " .. flavor .. "]", function()
+      reload_router(flavor)
+
       local MESSAGE = "echo, ping, pong. echo, ping, pong. echo, ping, pong.\n"
       lazy_setup(function()
         local bp = helpers.get_db_utils(strategy, {
@@ -153,7 +193,7 @@ for _, strategy in helpers.each_strategy() do
           protocol = "tcp",
         })
 
-        assert(bp.routes:insert {
+        assert(bp.routes:insert(gen_route(flavor, {
           destinations = {
             { port = 19000 },
           },
@@ -161,9 +201,9 @@ for _, strategy in helpers.each_strategy() do
             "tcp",
           },
           service = service,
-        })
+        })))
 
-        assert(bp.routes:insert {
+        assert(bp.routes:insert(gen_route(flavor, {
           protocols = { "tcp" },
           service   = service,
           destinations = {
@@ -174,9 +214,9 @@ for _, strategy in helpers.each_strategy() do
             { ip = "0.0.0.0" },
             { port = 19004 },
           }
-        })
+        })))
 
-        assert(bp.routes:insert {
+        assert(bp.routes:insert(gen_route(flavor, {
           protocols = { "tcp" },
           service   = service,
           destinations = {
@@ -187,9 +227,9 @@ for _, strategy in helpers.each_strategy() do
             { ip = "0.0.0.0" },
             { port = 19004 },
           }
-        })
+        })))
 
-        assert(bp.routes:insert {
+        assert(bp.routes:insert(gen_route(flavor, {
           protocols = { "tcp" },
           service   = service,
           destinations = {
@@ -200,9 +240,9 @@ for _, strategy in helpers.each_strategy() do
             { ip = "0.0.0.0" },
             { port = 19004 },
           }
-        })
+        })))
 
-        assert(bp.routes:insert {
+        assert(bp.routes:insert(gen_route(flavor, {
           protocols = { "tcp" },
           service   = service,
           destinations = {
@@ -213,9 +253,9 @@ for _, strategy in helpers.each_strategy() do
             { ip = "0.0.0.0" },
             { port = 19004 },
           }
-        })
+        })))
 
-        assert(bp.routes:insert {
+        assert(bp.routes:insert(gen_route(flavor, {
           protocols = { "tcp" },
           service   = service,
           destinations = {
@@ -226,9 +266,10 @@ for _, strategy in helpers.each_strategy() do
             { ip = "0.0.0.0" },
             { port = 19004 },
           }
-        })
+        })))
 
         assert(helpers.start_kong({
+          router_flavor = flavor,
           database      = strategy,
           stream_listen = helpers.get_proxy_ip(false) .. ":19000, " ..
                           helpers.get_proxy_ip(false) .. ":18000, " ..
@@ -282,3 +323,4 @@ for _, strategy in helpers.each_strategy() do
     end)
   end
 end
+end   -- for flavor
