Msaroto/asw

feat(core): Admin GUI integration
Serve static assets from Canopy project

Changes include:
* new nginx config template for static file serving
* new configuration items under admin_gui_* namespace
* bundle canopy assets during the build stage
* more CORS support for Admin API
* tests and more tests

kong/init.lua
@@ -90,6 +90,7 @@ local table_new = require "table.new"
 local utils = require "kong.tools.utils"
 local constants = require "kong.constants"
 local get_ctx_table = require("resty.core.ctx").get_ctx_table
+local admin_gui = require "kong.admin_gui"
 
 
 local kong             = kong
@@ -1582,7 +1583,7 @@ local function serve_content(module, options)
 
   options = options or {}
 
-  header["Access-Control-Allow-Origin"] = options.allow_origin or "*"
+  ngx.header["Access-Control-Allow-Origin"] = ngx.req.get_headers()["Origin"] or "*"
 
   lapis.serve(module)
 
@@ -1643,6 +1644,20 @@ function Kong.admin_header_filter()
   --ctx.KONG_ADMIN_HEADER_FILTER_TIME = ctx.KONG_ADMIN_HEADER_FILTER_ENDED_AT - ctx.KONG_ADMIN_HEADER_FILTER_START
 end
 
+function Kong.admin_gui_kconfig_content()
+  local content, err = kong.cache:get(
+    constants.ADMIN_GUI_K_CONFIG_JS_CACHE_KEY,
+    nil,
+    admin_gui.generate_kconfig,
+    kong.configuration
+  )
+  if err then
+    kong.log.err("error occurred while retrieving admin gui config `kconfig.js` from cache", err)
+    kong.response.exit(500, { message = "An unexpected error occurred" })
+  else
+    ngx.say(content)
+  end
+end
 
 function Kong.status_content()
   return serve_content("kong.status")

spec/01-unit/29-admin_gui/01-admin_gui_template_spec.lua
@@ -1,0 +1,188 @@
+local helpers        = require "spec.helpers"
+local admin_gui      = require "kong.admin_gui"
+local conf_loader    = require "kong.conf_loader"
+local prefix_handler = require "kong.cmd.utils.prefix_handler"
+
+local exists = helpers.path.exists
+
+describe("admin_gui template", function()
+  local conf = assert(conf_loader(helpers.test_conf_path))
+
+  it("auto-generates SSL certificate and key", function()
+    assert(prefix_handler.gen_default_ssl_cert(conf, "admin_gui"))
+    assert(exists(conf.admin_gui_ssl_cert_default))
+    assert(exists(conf.admin_gui_ssl_cert_key_default))
+  end)
+
+  it("does not re-generate if they already exist", function()
+    assert(prefix_handler.gen_default_ssl_cert(conf, "admin_gui"))
+    local cer = helpers.file.read(conf.admin_gui_ssl_cert_default)
+    local key = helpers.file.read(conf.admin_gui_ssl_cert_key_default)
+    assert(prefix_handler.gen_default_ssl_cert(conf, "admin_gui"))
+    assert.equal(cer, helpers.file.read(conf.admin_gui_ssl_cert_default))
+    assert.equal(key, helpers.file.read(conf.admin_gui_ssl_cert_key_default))
+  end)
+
+  it("generates a different SSL certificate and key from the RESTful API", function()
+    assert(prefix_handler.gen_default_ssl_cert(conf, "admin_gui"))
+    local cer, key = {}, {}
+    cer[1] = helpers.file.read(conf.admin_gui_ssl_cert_default)
+    key[1] = helpers.file.read(conf.admin_gui_ssl_cert_key_default)
+    assert(prefix_handler.gen_default_ssl_cert(conf, "admin"))
+    cer[2] = helpers.file.read(conf.admin_ssl_cert_default)
+    key[2] = helpers.file.read(conf.admin_ssl_cert_key_default)
+    assert.not_equals(cer[1], cer[2])
+    assert.not_equals(key[1], key[2])
+  end)
+
+  describe("admin_gui.generate_kconfig() - proxied", function()
+    local conf = {
+      admin_gui_url = "http://0.0.0.0:8002",
+      admin_gui_api_url = "https://admin-reference.kong-cloud.com",
+      proxy_url = "http://0.0.0.0:8000",
+      admin_gui_listeners = {
+        {
+          ip = "0.0.0.0",
+          port = 8002,
+          ssl = false,
+        },
+        {
+          ip = "0.0.0.0",
+          port = 8445,
+          ssl = true,
+        },
+      },
+      admin_listeners = {
+        {
+          ip = "0.0.0.0",
+          port = 8001,
+          ssl = false,
+        },
+        {
+          ip = "0.0.0.0",
+          port = 8444,
+          ssl = true,
+        }
+      },
+      proxy_listeners = {
+        {
+          ip = "0.0.0.0",
+          port = 8000,
+          ssl = false,
+        },
+        {
+          ip = "0.0.0.0",
+          port = 8443,
+          ssl = true,
+        }
+      },
+      admin_gui_path = '/canopy'
+    }
+
+    it("should generates the appropriate kconfig", function()
+      local kconfig_content = admin_gui.generate_kconfig(conf)
+
+      assert.matches("'ADMIN_GUI_URL': 'http://0.0.0.0:8002'", kconfig_content, nil, true)
+      assert.matches("'ADMIN_GUI_PATH': '/canopy'", kconfig_content, nil, true)
+      assert.matches("'ADMIN_GUI_API_URL': 'https://admin-reference.kong-cloud.com'", kconfig_content, nil, true)
+      assert.matches("'ADMIN_API_PORT': '8001'", kconfig_content, nil, true)
+      assert.matches("'ADMIN_API_SSL_PORT': '8444'", kconfig_content, nil, true)
+    end)
+
+    it("should regenerates the appropriate kconfig from another call", function()
+      local new_conf = conf
+
+      -- change configuration values
+      new_conf.admin_gui_url = 'http://admin-test.example.com'
+      new_conf.admin_gui_path = '/canopy'
+      new_conf.admin_gui_api_url = 'http://localhost:8001'
+      new_conf.proxy_url = 'http://127.0.0.1:8000'
+
+      -- regenerate kconfig
+      local new_content = admin_gui.generate_kconfig(new_conf)
+
+      -- test configuration values against template
+      assert.matches("'ADMIN_GUI_URL': 'http://admin-test.example.com'", new_content, nil, true)
+      assert.matches("'ADMIN_GUI_PATH': '/canopy'", new_content, nil, true)
+      assert.matches("'ADMIN_GUI_API_URL': 'http://localhost:8001'", new_content, nil, true)
+      assert.matches("'ADMIN_API_PORT': '8001'", new_content, nil, true)
+      assert.matches("'ADMIN_API_SSL_PORT': '8444'", new_content, nil, true)
+    end)
+  end)
+
+  describe("admin_gui.generate_kconfig() - not proxied", function()
+    local conf = {
+      admin_gui_url = "http://0.0.0.0:8002",
+      proxy_url = "http://0.0.0.0:8000",
+      admin_gui_api_url = "0.0.0.0:8001",
+      anonymous_reports = false,
+      admin_gui_listeners = {
+        {
+          ip = "0.0.0.0",
+          port = 8002,
+          ssl = false,
+        },
+        {
+          ip = "0.0.0.0",
+          port = 8445,
+          ssl = true,
+        },
+      },
+      admin_listeners = {
+        {
+          ip = "0.0.0.0",
+          port = 8001,
+          ssl = false,
+        },
+        {
+          ip = "0.0.0.0",
+          port = 8444,
+          ssl = true,
+        }
+      },
+      proxy_listeners = {
+        {
+          ip = "0.0.0.0",
+          port = 8000,
+          ssl = false,
+        },
+        {
+          ip = "0.0.0.0",
+          port = 8443,
+          ssl = true,
+        }
+      },
+      rbac = "off",
+      rbac_auth_header = 'Kong-Admin-Token',
+    }
+
+    it("should generates the appropriate kconfig", function()
+      local kconfig_content = admin_gui.generate_kconfig(conf)
+
+      assert.matches("'ADMIN_GUI_URL': 'http://0.0.0.0:8002'", kconfig_content, nil, true)
+      assert.matches("'ADMIN_GUI_API_URL': '0.0.0.0:8001'", kconfig_content, nil, true)
+      assert.matches("'ADMIN_API_PORT': '8001'", kconfig_content, nil, true)
+      assert.matches("'ADMIN_API_SSL_PORT': '8444'", kconfig_content, nil, true)
+      assert.matches("'ANONYMOUS_REPORTS': 'false'", kconfig_content, nil, true)
+    end)
+
+    it("should regenerates the appropriate kconfig from another call", function()
+      local new_conf = conf
+
+      -- change configuration values
+      new_conf.admin_gui_url = 'http://admin-test.example.com'
+      new_conf.proxy_url = 'http://127.0.0.1:8000'
+      new_conf.anonymous_reports = true
+
+      -- regenerate kconfig
+      local new_content = admin_gui.generate_kconfig(new_conf)
+
+      -- test configuration values against template
+      assert.matches("'ADMIN_GUI_URL': 'http://admin-test.example.com'", new_content, nil, true)
+      assert.matches("'ADMIN_GUI_API_URL': '0.0.0.0:8001'", new_content, nil, true)
+      assert.matches("'ADMIN_API_PORT': '8001'", new_content, nil, true)
+      assert.matches("'ADMIN_API_SSL_PORT': '8444'", new_content, nil, true)
+      assert.matches("'ANONYMOUS_REPORTS': 'true'", new_content, nil, true)
+    end)
+  end)
+end)

HimajPatil/kong

kong/init.lua
@@ -91,6 +91,7 @@ local table_new = require "table.new"
 local utils = require "kong.tools.utils"
 local constants = require "kong.constants"
 local get_ctx_table = require("resty.core.ctx").get_ctx_table
+local admin_gui = require "kong.admin_gui"
 
 
 local kong             = kong
@@ -1604,7 +1605,7 @@ local function serve_content(module, options)
 
   options = options or {}
 
-  header["Access-Control-Allow-Origin"] = options.allow_origin or "*"
+  ngx.header["Access-Control-Allow-Origin"] = ngx.req.get_headers()["Origin"] or "*"
 
   lapis.serve(module)
 
@@ -1665,6 +1666,20 @@ function Kong.admin_header_filter()
   --ctx.KONG_ADMIN_HEADER_FILTER_TIME = ctx.KONG_ADMIN_HEADER_FILTER_ENDED_AT - ctx.KONG_ADMIN_HEADER_FILTER_START
 end
 
+function Kong.admin_gui_kconfig_content()
+  local content, err = kong.cache:get(
+    constants.ADMIN_GUI_KCONFIG_CACHE_KEY,
+    nil,
+    admin_gui.generate_kconfig,
+    kong.configuration
+  )
+  if err then
+    kong.log.err("error occurred while retrieving admin gui config `kconfig.js` from cache", err)
+    kong.response.exit(500, { message = "An unexpected error occurred" })
+  else
+    ngx.say(content)
+  end
+end
 
 function Kong.status_content()
   return serve_content("kong.status")

spec/01-unit/29-admin_gui/01-admin_gui_template_spec.lua
@@ -1,0 +1,192 @@
+local helpers        = require "spec.helpers"
+local admin_gui      = require "kong.admin_gui"
+local conf_loader    = require "kong.conf_loader"
+local prefix_handler = require "kong.cmd.utils.prefix_handler"
+
+local exists = helpers.path.exists
+
+describe("admin_gui template", function()
+  local conf = assert(conf_loader(helpers.test_conf_path))
+
+  it("auto-generates SSL certificate and key", function()
+    assert(prefix_handler.gen_default_ssl_cert(conf, "admin_gui"))
+    assert(exists(conf.admin_gui_ssl_cert_default))
+    assert(exists(conf.admin_gui_ssl_cert_key_default))
+  end)
+
+  it("does not re-generate if they already exist", function()
+    assert(prefix_handler.gen_default_ssl_cert(conf, "admin_gui"))
+    local cer = helpers.file.read(conf.admin_gui_ssl_cert_default)
+    local key = helpers.file.read(conf.admin_gui_ssl_cert_key_default)
+    assert(prefix_handler.gen_default_ssl_cert(conf, "admin_gui"))
+    assert.equal(cer, helpers.file.read(conf.admin_gui_ssl_cert_default))
+    assert.equal(key, helpers.file.read(conf.admin_gui_ssl_cert_key_default))
+  end)
+
+  it("generates a different SSL certificate and key from the RESTful API", function()
+    assert(prefix_handler.gen_default_ssl_cert(conf, "admin_gui"))
+    local cer, key = {}, {}
+    cer[1] = helpers.file.read(conf.admin_gui_ssl_cert_default)
+    key[1] = helpers.file.read(conf.admin_gui_ssl_cert_key_default)
+    assert(prefix_handler.gen_default_ssl_cert(conf, "admin"))
+    cer[2] = helpers.file.read(conf.admin_ssl_cert_default)
+    key[2] = helpers.file.read(conf.admin_ssl_cert_key_default)
+    assert.not_equals(cer[1], cer[2])
+    assert.not_equals(key[1], key[2])
+  end)
+
+  describe("admin_gui.generate_kconfig() - proxied", function()
+    local conf = {
+      admin_gui_url = "http://0.0.0.0:8002",
+      admin_gui_api_url = "https://admin-reference.kong-cloud.com",
+      proxy_url = "http://0.0.0.0:8000",
+      admin_gui_listeners = {
+        {
+          ip = "0.0.0.0",
+          port = 8002,
+          ssl = false,
+        },
+        {
+          ip = "0.0.0.0",
+          port = 8445,
+          ssl = true,
+        },
+      },
+      admin_listeners = {
+        {
+          ip = "0.0.0.0",
+          port = 8001,
+          ssl = false,
+        },
+        {
+          ip = "0.0.0.0",
+          port = 8444,
+          ssl = true,
+        }
+      },
+      proxy_listeners = {
+        {
+          ip = "0.0.0.0",
+          port = 8000,
+          ssl = false,
+        },
+        {
+          ip = "0.0.0.0",
+          port = 8443,
+          ssl = true,
+        }
+      },
+      admin_gui_path = '/manager'
+    }
+
+    it("should generates the appropriate kconfig", function()
+      local kconfig_content = admin_gui.generate_kconfig(conf)
+
+      assert.matches("'ADMIN_GUI_URL': 'http://0.0.0.0:8002'", kconfig_content, nil, true)
+      assert.matches("'ADMIN_GUI_PATH': '/manager'", kconfig_content, nil, true)
+      assert.matches("'ADMIN_API_URL': 'https://admin-reference.kong-cloud.com'", kconfig_content, nil, true)
+      assert.matches("'ADMIN_API_PORT': '8001'", kconfig_content, nil, true)
+      assert.matches("'ADMIN_API_SSL_PORT': '8444'", kconfig_content, nil, true)
+      assert.matches("'KONG_EDITION': 'community'", kconfig_content, nil, true)
+    end)
+
+    it("should regenerates the appropriate kconfig from another call", function()
+      local new_conf = conf
+
+      -- change configuration values
+      new_conf.admin_gui_url = 'http://admin-test.example.com'
+      new_conf.admin_gui_path = '/manager'
+      new_conf.admin_gui_api_url = 'http://localhost:8001'
+      new_conf.proxy_url = 'http://127.0.0.1:8000'
+
+      -- regenerate kconfig
+      local new_content = admin_gui.generate_kconfig(new_conf)
+
+      -- test configuration values against template
+      assert.matches("'ADMIN_GUI_URL': 'http://admin-test.example.com'", new_content, nil, true)
+      assert.matches("'ADMIN_GUI_PATH': '/manager'", new_content, nil, true)
+      assert.matches("'ADMIN_API_URL': 'http://localhost:8001'", new_content, nil, true)
+      assert.matches("'ADMIN_API_PORT': '8001'", new_content, nil, true)
+      assert.matches("'ADMIN_API_SSL_PORT': '8444'", new_content, nil, true)
+      assert.matches("'KONG_EDITION': 'community'", new_content, nil, true)
+    end)
+  end)
+
+  describe("admin_gui.generate_kconfig() - not proxied", function()
+    local conf = {
+      admin_gui_url = "http://0.0.0.0:8002",
+      proxy_url = "http://0.0.0.0:8000",
+      admin_gui_api_url = "0.0.0.0:8001",
+      anonymous_reports = false,
+      admin_gui_listeners = {
+        {
+          ip = "0.0.0.0",
+          port = 8002,
+          ssl = false,
+        },
+        {
+          ip = "0.0.0.0",
+          port = 8445,
+          ssl = true,
+        },
+      },
+      admin_listeners = {
+        {
+          ip = "0.0.0.0",
+          port = 8001,
+          ssl = false,
+        },
+        {
+          ip = "0.0.0.0",
+          port = 8444,
+          ssl = true,
+        }
+      },
+      proxy_listeners = {
+        {
+          ip = "0.0.0.0",
+          port = 8000,
+          ssl = false,
+        },
+        {
+          ip = "0.0.0.0",
+          port = 8443,
+          ssl = true,
+        }
+      },
+      rbac = "off",
+      rbac_auth_header = 'Kong-Admin-Token',
+    }
+
+    it("should generates the appropriate kconfig", function()
+      local kconfig_content = admin_gui.generate_kconfig(conf)
+
+      assert.matches("'ADMIN_GUI_URL': 'http://0.0.0.0:8002'", kconfig_content, nil, true)
+      assert.matches("'ADMIN_API_URL': '0.0.0.0:8001'", kconfig_content, nil, true)
+      assert.matches("'ADMIN_API_PORT': '8001'", kconfig_content, nil, true)
+      assert.matches("'ADMIN_API_SSL_PORT': '8444'", kconfig_content, nil, true)
+      assert.matches("'ANONYMOUS_REPORTS': 'false'", kconfig_content, nil, true)
+      assert.matches("'KONG_EDITION': 'community'", kconfig_content, nil, true)
+    end)
+
+    it("should regenerates the appropriate kconfig from another call", function()
+      local new_conf = conf
+
+      -- change configuration values
+      new_conf.admin_gui_url = 'http://admin-test.example.com'
+      new_conf.proxy_url = 'http://127.0.0.1:8000'
+      new_conf.anonymous_reports = true
+
+      -- regenerate kconfig
+      local new_content = admin_gui.generate_kconfig(new_conf)
+
+      -- test configuration values against template
+      assert.matches("'ADMIN_GUI_URL': 'http://admin-test.example.com'", new_content, nil, true)
+      assert.matches("'ADMIN_API_URL': '0.0.0.0:8001'", new_content, nil, true)
+      assert.matches("'ADMIN_API_PORT': '8001'", new_content, nil, true)
+      assert.matches("'ADMIN_API_SSL_PORT': '8444'", new_content, nil, true)
+      assert.matches("'ANONYMOUS_REPORTS': 'true'", new_content, nil, true)
+      assert.matches("'KONG_EDITION': 'community'", new_content, nil, true)
+    end)
+  end)
+end)